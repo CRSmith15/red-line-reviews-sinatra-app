8802586a348e8bc7f1d86db9ec8cb16d17dae0ca
Chance Smith <chancesmith97@hotmail.com>
2020-10-16 00:09:33 +0000

    added signup form and functionality + show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,41 @@
 class UsersController < ApplicationController
-  
-  get "/login" do 
+
+  get "/login" do
     erb :"login"
   end
-  
-  post "/login" do 
+
+  post "/login" do
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id 
+      session[:user_id] = user.id
       redirect "/users/#{user.id}"
     else
       redirect "/login"
     end
   end
-  
-  get "/users/:id" do 
+
+  get "/users/:id" do
+    @user = User.find_by(id: params[:id])
+    erb :"/users/show"
+  end
+
+  get "/signup" do
+    erb :"/users/signup"
     "show page for users"
+  end
+
+  get "/signup" do
+    erb :"/signup"
+  end
+
+  post "/users" do
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
   end
 end
