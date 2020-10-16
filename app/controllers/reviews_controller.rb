6d305ba0144573d961426e4cf14d2475143416f8
Chance Smith <chancesmith97@hotmail.com>
2020-10-16 03:00:16 +0000

    added a form to create a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,24 @@
 class ReviewsController < ApplicationController
 
-  get '/reviews' do
+  get "/reviews" do
     @reviews = Review.all
     erb :'reviews/index'
   end
 
-  get '/reviews/:id' do
+  get "/reviews/new" do
+    erb :"/reviews/new"
+  end
+
+  get "/reviews/:id" do
     @review = Review.find(params[:id])
     erb :"reviews/show"
-
   end
+
+  post "/reviews" do
+    rev = Review.create(game_title: params[:game_title], score: params[:score], description: params[:description], user_id: current_user.id)
+    redirect "/reviews/#{rev.id}"
+  end
+
+
 
 end
